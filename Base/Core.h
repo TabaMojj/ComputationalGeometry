f7f6b9008d5b1b97f67e34a81003ee95db3d97ea
Mojtaba Mir Yaghoobzdeh <mirmojtaba21@gmail.com>
2024-03-30 02:21:50 +0330

    add _xor

--- Base/Core.h
+++ Base/Core.h
@@ -6,18 +6,28 @@
 
 #include <cmath>
 
-#define TOLERANCE 0.0000001
-
 #ifndef COMPUTATIONALGEOMETRY_CORE_H
 #define COMPUTATIONALGEOMETRY_CORE_H
 
 #endif //COMPUTATIONALGEOMETRY_CORE_H
 
+namespace CG {
 
-static bool isEqualID(double x, double y){
-    return fabs(x-y) < TOLERANCE;
+    #define TOLERANCE 0.0000001
+
+    static bool isEqualID(double x, double y){
+        return fabs(x-y) < TOLERANCE;
+    }
+
+    enum RELATIVE_POSITION {
+        LEFT, RIGHT, BEHIND, BEYOND, BETWEEN, ORIGIN, DESTINATION
+    };
+
+    static bool _xor(bool x, bool y) {
+        return x ^ y;
+    }
+
 }
 
-enum RELATIVE_POSITION {
-    LEFT, RIGHT, BEHIND, BEYOND, BETWEEN, ORIGIN, DESTINATION
-};
+
+
