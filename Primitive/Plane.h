a18f78a093ce7c28ba0dccee3989d632358ccfd4
Mojtaba Mir Yaghoobzdeh <mirmojtaba21@gmail.com>
2024-03-29 23:57:01 +0330

    add Plane header

---
+++ Primitive/Plane.h
@@ -0,0 +1,33 @@
+//
+// Created by mojtaba on 3/29/24.
+//
+#pragma once
+
+#include "../Base/Vector.h"
+#include "Point.h"
+
+#ifndef COMPUTATIONALGEOMETRY_PLANE_H
+#define COMPUTATIONALGEOMETRY_PLANE_H
+
+#endif //COMPUTATIONALGEOMETRY_PLANE_H
+
+namespace CG {
+    template<class coordinate_type>
+    class Plane {
+        Vector3f normal;
+        float d = 0;
+
+    public:
+        Plane() = default;
+
+        Plane(Vector3f &_normal, float _constant):normal(_normal), d(_constant){}
+
+        Plane(Point3d &_p1, Point3d &_p2, Point3d &_p3) {
+            auto v12 = _p2 - _p1;
+            auto v13 = _p3 - _p1;
+            normal = crossProduct3D(v12, v13);
+            d = dotProduct(normal, _p1);
+        }
+    };
+
+}
